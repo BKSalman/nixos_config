1d4d89cd95d5e31f068e65088fe3ede47d19baf4
BKSalman <salman.f.abuhaimed@gmail.com>
2023-09-10 03:35:18 +0300

    add bash aliases

--- bash/default.nix
+++ bash/default.nix
@@ -5,6 +5,8 @@
     shellAliases = {
       nd = "nix develop";
       sk = "screenkey -g 300x300+2700+790";
+      jc = "journalctl --output cat";
+      switch = "sudo nixo-rebuild switch --flake .#";
     };
     bashrcExtra = builtins.readFile ./.bashrc;
   };
