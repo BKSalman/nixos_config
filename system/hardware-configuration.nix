f63c0f76da9c536a443f1c3c34312d18a683f078
BKSalman <salman.f.abuhaimed@gmail.com>
2023-09-11 17:08:17 +0300

    replace /media/nextcloud with /media/extra

--- system/hardware-configuration.nix
+++ system/hardware-configuration.nix
@@ -43,7 +43,7 @@
       fsType = "ext4";
     };
 
-  fileSystems."/media/nextcloud" =
+  fileSystems."/media/extra" =
     { device = "/dev/disk/by-uuid/78ec7175-a339-4330-a463-d6516b6f169f";
       fsType = "ext4";
     };
