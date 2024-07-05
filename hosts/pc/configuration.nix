9b7201ea21e4eea607e28cb5b37d366b55ed7d78
Salman Abuhaimed <salman.f.abuhaimed@gmail.com>
2024-07-05 15:20:27 +0300

    94 current  2024-07-05 15:20:24  24.11.20240613.e9ee548  6.8.12                          *

--- hosts/pc/configuration.nix
+++ hosts/pc/configuration.nix
@@ -214,6 +214,9 @@
   services.flatpak.enable = true;
   xdg.portal = {
     enable = true;
+    extraPortals = with pkgs; [
+      xdg-desktop-portal
+    ];
     config.common.default = "*";
   };
 
