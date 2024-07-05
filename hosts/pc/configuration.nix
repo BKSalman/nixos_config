52bfe4f8809bdb32b8a1519eee439baddbace80d
Salman Abuhaimed <salman.f.abuhaimed@gmail.com>
2024-07-05 15:53:19 +0300

    97 current  2024-07-05 15:53:15  24.11.20240613.e9ee548  6.8.12                          *

--- hosts/pc/configuration.nix
+++ hosts/pc/configuration.nix
@@ -217,6 +217,10 @@
     extraPortals = with pkgs; [
       xdg-desktop-portal-gtk
     ];
+    config.common.default = [
+      "jay"
+      "gtk"
+    ];
   };
 
   environment.sessionVariables = {
