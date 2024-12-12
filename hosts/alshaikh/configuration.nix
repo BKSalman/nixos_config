ae292138d372843cd3250c6df4effd63ec843e14
Salman Abuhaimed <salman.f.abuhaimed@gmail.com>
2024-12-12 17:35:18 +0300

    12 current  2024-12-12 17:35:17  25.05.20241125.4633a7c        6.6.63                          *

--- hosts/alshaikh/configuration.nix
+++ hosts/alshaikh/configuration.nix
@@ -129,6 +129,8 @@
     allowedUDPPorts = [config.services.tailscale.port];
   };
 
+  environment.etc."makepkg.conf".source = "${pkgs.pacman}/etc/makepkg.conf";
+
   # This value determines the NixOS release from which the default
   # settings for stateful data, like file locations and database versions
   # on your system were taken. It‘s perfectly fine and recommended to leave
