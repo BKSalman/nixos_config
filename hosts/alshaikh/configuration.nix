4f268cdad0859f99ad0f772ad6e7aef70b6948c6
Salman Abuhaimed <salman.f.abuhaimed@gmail.com>
2025-01-01 19:51:57 +0300

    23 current  2025-01-01 19:51:55  25.05.20241125.4633a7c        6.6.63                          *

--- hosts/alshaikh/configuration.nix
+++ hosts/alshaikh/configuration.nix
@@ -21,6 +21,8 @@
   # networking.proxy.default = "http://user:password@proxy:port/";
   # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
 
+  wayland.enable = true;
+
   # Enable networking
   networking.networkmanager.enable = true;
 
