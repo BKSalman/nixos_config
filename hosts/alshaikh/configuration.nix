086cb2b84dca8cb7438316b36a174288f6a51732
Salman Abuhaimed <salman.f.abuhaimed@gmail.com>
2025-01-02 19:57:22 +0300

    25 current  2025-01-02 19:57:21  25.05.20241125.4633a7c        6.6.63                          *

--- hosts/alshaikh/configuration.nix
+++ hosts/alshaikh/configuration.nix
@@ -120,6 +120,7 @@
   # List packages installed in system profile. To search, run:
   # $ nix search wget
   environment.systemPackages = with pkgs; [
+    tree
     sops
     rpcs3
     dolphin-emu
