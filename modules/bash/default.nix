db3c1a70fb9f526d839b592816c7927dc4cf1b0e
Salman Abuhaimed <salman.f.abuhaimed@gmail.com>
2025-01-29 23:01:09 +0300

    180 current  2025-01-29 23:01:01  25.05.20250112.ed4a395  6.12.9                          *

--- modules/bash/default.nix
+++ modules/bash/default.nix
@@ -44,6 +44,7 @@
       MANPAGER = "sh -c 'col -bx | bat -l man -p'";
       MANROFFOPT = "-c";
       EDITOR = "hx";
+      STEEL_LSP_HOME = "/home/salman/.config/steel-lsp/";
     };
     enableCompletion = true;
     bashrcExtra = ''
