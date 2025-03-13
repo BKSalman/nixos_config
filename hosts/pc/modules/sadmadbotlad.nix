59be6a8a6f3d61795204aea72943601fc85ec6f8
Salman Abuhaimed <salman.f.abuhaimed@gmail.com>
2025-03-14 01:52:56 +0300

    fix sadmadbotlad service

--- hosts/pc/modules/sadmadbotlad.nix
+++ hosts/pc/modules/sadmadbotlad.nix
@@ -20,7 +20,7 @@
     systemd.user.services.sadmadbotlad = {
       wantedBy = ["default.target"];
       serviceConfig = {
-        script =
+        ExecStart =
           "${sadmadbotlad.packages.x86_64-linux.default}/bin/sadmadbotlad "
           + "-c ${config.sops.secrets.sadmadbotlad-config.path} "
           + "--commands-path ${sadmadbotlad.packages.x86_64-linux.default}/share/commands "
