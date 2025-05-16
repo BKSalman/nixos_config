37ee80a9a0971cd1bed5362c797ea4398fcf9429
Salman Abuhaimed <salman.f.abuhaimed@gmail.com>
2025-05-16 22:05:57 +0300

    202 current  2025-05-16 22:05:51  25.05.20250513.adaa24f  6.12.28                          *

--- modules/bash/default.nix
+++ modules/bash/default.nix
@@ -64,6 +64,15 @@
       fi
 
       export PATH="$PATH:$HOME/.cargo/bin"
+
+      function y() {
+          local tmp="$(mktemp -t "yazi-cwd.XXXXXX")" cwd
+          yazi "$@" --cwd-file="$tmp"
+          if cwd="$(command cat -- "$tmp")" && [ -n "$cwd" ] && [ "$cwd" != "$PWD" ]; then
+            builtin cd -- "$cwd"
+          fi
+          rm -f -- "$tmp"
+        }
     '';
   };
 }
