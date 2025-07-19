cb9f3792dc4d10a80428b4a2459509892e7b8c3e
Salman Abuhaimed <salman.f.abuhaimed@gmail.com>
2025-07-19 20:11:49 +0300

    231 2025-07-19 20:10:00 25.11.20250704.5c724ed 6.12.35

--- modules/helix/home.nix
+++ modules/helix/home.nix
@@ -5,7 +5,9 @@
   home.file.".config/helix/languages.toml".source = ./languages.toml;
   home.file.".config/helix/config.toml".source = ./config.toml;
   home.file.".config/helix/cogs/keymaps.scm".source = ./cogs/keymaps.scm;
-  home.file.".steel/cogs/package.scm".source = ./cogs/package.scm;
   home.file.".config/helix/init.scm".source = ./init.scm;
   home.file.".config/helix/helix.scm".source = ./helix.scm;
+
+  home.file.".steel/cogs/package.scm".source = ./cogs/package.scm;
+  home.file.".steel/cogs/picker.scm".source = ./cogs/picker.scm;
 }
