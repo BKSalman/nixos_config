7e288465e498c7ef8fe6e8288db9acf209cb9861
Salman Abuhaimed <salman.f.abuhaimed@gmail.com>
2025-08-07 23:26:36 +0300

    symlink helix config files instead
    this makes home-manager symlink my config files
    instead of managing them, which would make them readonly
    and would require rebuilding the config
    everytime I want to change something

--- modules/helix/home.nix
+++ modules/helix/home.nix
@@ -11,13 +11,11 @@
   programs.helix.enable = true;
 
   home.file.".config/helix/languages.toml".source = config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/nixos_config/modules/helix/languages.toml";
+  home.file.".config/helix/config.toml".source = config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/nixos_config/modules/helix/config.toml";
+  home.file.".config/helix/cogs/keymaps.scm".source = config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/nixos_config/modules/helix/cogs/keymaps.scm";
+  home.file.".config/helix/init.scm".source = config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/nixos_config/modules/helix/init.scm";
+  home.file.".config/helix/helix.scm".source = config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/nixos_config/modules/helix/helix.scm";
 
-  # home.file.".config/helix/languages.toml".source = ./languages.toml;
-  home.file.".config/helix/config.toml".source = ./config.toml;
-  home.file.".config/helix/cogs/keymaps.scm".source = ./cogs/keymaps.scm;
-  home.file.".config/helix/init.scm".source = ./init.scm;
-  home.file.".config/helix/helix.scm".source = ./helix.scm;
-
-  home.file.".steel/cogs/package.scm".source = ./cogs/package.scm;
-  home.file.".steel/cogs/picker.scm".source = ./cogs/picker.scm;
+  home.file.".steel/cogs/package.scm".source = config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/nixos_config/modules/helix/cogs/package.scm";
+  home.file.".steel/cogs/picker.scm".source = config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/nixos_config/modules/helix/cogs/picker.scm";
 }
