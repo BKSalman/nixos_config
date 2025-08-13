3c9c2eb90af1fe19f62ceafcd94f7add9f47bc5f
Salman Abuhaimed <salman.f.abuhaimed@gmail.com>
2025-08-13 23:30:39 +0300

    99 2025-08-13 23:29:03 25.11.20250803.5b09dc4 6.12.41

--- hosts/alshaikh/configuration.nix
+++ hosts/alshaikh/configuration.nix
@@ -158,6 +158,7 @@ in {
   # List packages installed in system profile. To search, run:
   # $ nix search wget
   environment.systemPackages = with pkgs; [
+    typst
     inotify-tools
     lsof
     fhs
