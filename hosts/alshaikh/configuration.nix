ad67e3db6227027d5ba1227a5d3e1de800d64e72
Salman Abuhaimed <salman.f.abuhaimed@gmail.com>
2025-09-19 12:49:06 +0300

    111 2025-09-19 00:27:29 25.11.20250814.fbcf476 6.12.41

--- hosts/alshaikh/configuration.nix
+++ hosts/alshaikh/configuration.nix
@@ -46,6 +46,8 @@ in {
   # networking.proxy.default = "http://user:password@proxy:port/";
   # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
 
+  jay.enable = true;
+
   niri.enable = false;
 
   cosmic.enable = false;
--- modules/jay/default.nix
+++ modules/jay/default.nix
@@ -4,36 +4,11 @@
   lib,
   ...
 }: let
-  jay-unstable = pkgs.jay.overrideAttrs (final: prev: rec {
-    version = "unstable";
-    src = pkgs.fetchFromGitHub {
-      owner = "mahkoh";
-      repo = "jay";
-      rev = "1ca5d43557e892749b2497cefc69675faa7290ba";
-      sha256 = "sha256-VAg59hmI38hJzkh/Vtv6LjrqQFLaq7rIGtk9sfQe1TA=";
-    };
-    cargoDeps = prev.cargoDeps.overrideAttrs (_: {
-      inherit src version;
-      outputHash = "sha256-ay9Usb+noXjWz6bEYd1YwkMC4Bn8ioux8WLuoCqw/Ec=";
-    });
-
-    postInstall = ''
-      install -D etc/jay.portal $out/share/xdg-desktop-portal/portals/jay.portal
-      install -D etc/jay-portals.conf $out/share/xdg-desktop-portal/jay-portals.conf
-    '';
-  });
-  jay-with-session = jay-unstable.overrideAttrs (final: prev: let
-    session = pkgs.writeText "jay" ''
-      [Desktop Entry]
-      Name=Jay
-      Exec=${jay-unstable}/bin/jay --log-level info run
-      Type=Application
-    '';
-  in {
+  jay-with-session = pkgs.jay.overrideAttrs (final: prev: {
     postInstall =
       prev.postInstall
       + ''
-        install -D ${session} $out/share/wayland-sessions/jay.desktop
+        install -D $src/etc/jay.desktop $out/share/wayland-sessions/jay.desktop
       '';
     passthru.providedSessions = ["jay"];
   });
