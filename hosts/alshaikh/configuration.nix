9bcd10bc3de6670345c421623dae492b0f366a5a
Salman Abuhaimed <salman.f.abuhaimed@gmail.com>
2025-10-30 21:10:58 +0300

    use podman instead of docker

--- hosts/alshaikh/configuration.nix
+++ hosts/alshaikh/configuration.nix
@@ -178,22 +178,22 @@ in {
   #     PCIE_ASPM_ON_BAT = "powersupersave";
   #   };
   # };
-  services.power-profiles-daemon.enable = false;
-  services.auto-cpufreq = {
-    enable = true;
-    settings = {
-      # https://github.com/AdnanHodzic/auto-cpufreq
-      battery = {
-        governor = "powersave";
-        turbo = "never";
-      };
-      charger = {
-        governor = "performance";
-        turbo = "auto";
-      };
-    };
-  };
-  powerManagement.powertop.enable = true;
+  # services.power-profiles-daemon.enable = false;
+  # services.auto-cpufreq = {
+  #   enable = true;
+  #   settings = {
+  #     # https://github.com/AdnanHodzic/auto-cpufreq
+  #     battery = {
+  #       governor = "powersave";
+  #       turbo = "never";
+  #     };
+  #     charger = {
+  #       governor = "performance";
+  #       turbo = "auto";
+  #     };
+  #   };
+  # };
+  # powerManagement.powertop.enable = true;
 
   # List packages installed in system profile. To search, run:
   # $ nix search wget
@@ -269,28 +269,28 @@ in {
 
   virtualisation = {
     # Podman
-    # oci-containers.backend = "podman";
-    # podman = {
-    #   enable = true;
-    #   # Create a `docker` alias for podman, to use it as a drop-in replacement
-    #   dockerCompat = true;
-    #   dockerSocket.enable = true;
-
-    #   # Required for containers under podman-compose to be able to talk to each other.
-    #   defaultNetwork.settings = {
-    #     dns_enabled = true;
-    #   };
-    # };
-
-    # Docker
-    oci-containers.backend = "docker";
-    docker = {
+    oci-containers.backend = "podman";
+    podman = {
       enable = true;
-      rootless = {
-        enable = true;
-        setSocketVariable = true;
+      # Create a `docker` alias for podman, to use it as a drop-in replacement
+      dockerCompat = true;
+      dockerSocket.enable = true;
+
+      # Required for containers under podman-compose to be able to talk to each other.
+      defaultNetwork.settings = {
+        dns_enabled = true;
       };
     };
+
+    # Docker
+    # oci-containers.backend = "docker";
+    # docker = {
+    #   enable = true;
+    #   rootless = {
+    #     enable = true;
+    #     setSocketVariable = true;
+    #   };
+    # };
   };
 
   uxplay.enable = true;
