0d9fdb7ffd408be6c2e738ea9baf07192a7b2037
Salman Abuhaimed <salman.f.abuhaimed@gmail.com>
2025-11-15 00:51:00 +0300

    a

--- hosts/alshaikh/configuration.nix
+++ hosts/alshaikh/configuration.nix
@@ -198,6 +198,7 @@ in {
   # List packages installed in system profile. To search, run:
   # $ nix search wget
   environment.systemPackages = with pkgs; [
+    claude-code
     iotop
     kdePackages.partitionmanager
     nixd
