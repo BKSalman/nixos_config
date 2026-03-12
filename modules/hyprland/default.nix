65bb822119e00c51e4a3540678d15b86dbbfcf7e
Salman Abuhaimed <salman.f.abuhaimed@gmail.com>
2026-03-12 23:54:52 +0300

    stuff

--- modules/hyprland/default.nix
+++ modules/hyprland/default.nix
@@ -11,10 +11,27 @@
   config = lib.mkIf config.hyprland.enable {
     programs.hyprland = {
       enable = true;
+      withUWSM = true;
+    };
+
+    xdg.portal = {
+      enable = true;
+      extraPortals = with pkgs; [ xdg-desktop-portal-hyprland ];
     };
 
     environment.etc."xdg/menus/applications.menu".source =
       "${pkgs.kdePackages.plasma-workspace}/etc/xdg/menus/plasma-applications.menu";
+
+    environment.etc = {
+      "xdg/qt5ct/qt5ct.conf".text = ''
+        [Appearance]
+        style=Breeze
+      '';
+      "xdg/qt6ct/qt6ct.conf".text = ''
+        [Appearance]
+        style=Breeze
+      '';
+    };
 
     environment.systemPackages = with pkgs; [
       (rofi.override {plugins = [pkgs.rofi-emoji];})
@@ -23,6 +40,9 @@
       grimblast
       hyprpicker
       cliphist
+      libsForQt5.qt5ct
+      kdePackages.qt6ct
+      kdePackages.breeze
     ];
 
     services.displayManager.sessionPackages = [
