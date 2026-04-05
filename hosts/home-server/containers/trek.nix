e2313d21f95b03891158a7ca75c0b0f88bfedd0e
Salman Abuhaimed <salman.f.abuhaimed@gmail.com>
2026-04-06 00:11:21 +0300

    update trek to 2.9.4

--- hosts/home-server/containers/trek.nix
+++ hosts/home-server/containers/trek.nix
@@ -8,7 +8,7 @@
 
   # Docker container
   virtualisation.oci-containers.containers.trek = {
-    image = "mauriceboe/trek:2.9.0";
+    image = "mauriceboe/trek:2.9.4";
     ports = ["3001:3000"];
     volumes = [
       "/mnt/trek/data:/app/data"
