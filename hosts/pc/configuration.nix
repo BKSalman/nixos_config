11f8afb195ae240a00ec1c2b630d00549bca7a84
Salman Abuhaimed <salman.f.abuhaimed@gmail.com>
2026-05-09 19:43:48 +0300

    steam stuff

--- hosts/pc/configuration.nix
+++ hosts/pc/configuration.nix
@@ -494,8 +494,15 @@
 
   # Steam
   hardware.steam-hardware.enable = true;
-  programs.steam.enable = true;
-  programs.steam.remotePlay.openFirewall = true;
+  programs.steam = {
+    enable = true;
+    remotePlay.openFirewall = true;
+    dedicatedServer.openFirewall = true;
+    localNetworkGameTransfers.openFirewall = true;
+    extraCompatPackages = with pkgs; [
+      proton-ge-bin
+    ];
+  };
 
   services.mpd.enable = true;
 
