99e4a3069ec5c5cada097940170a4aed7c72bd6f
Salman Abuhaimed <salman.f.abuhaimed@gmail.com>
2026-08-02 20:16:11 +0300

    vaultwarden: proxy websockets in nginx

--- hosts/home-server/modules/vaultwarden.nix
+++ hosts/home-server/modules/vaultwarden.nix
@@ -25,6 +25,7 @@ in {
     forceSSL = true;
     locations."/" = {
       proxyPass = "http://127.0.0.1:${toString config.services.vaultwarden.config.ROCKET_PORT}";
+      proxyWebsockets = true;
     };
   };
 
