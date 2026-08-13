cf8254a788ef0b950b609d3d3ab71100dcee6e6f
Salman Abuhaimed <salman.f.abuhaimed@gmail.com>
2026-08-13 23:30:33 +0300

    update trek to 4.0.0-pre.1

--- hosts/home-server/containers/trek.nix
+++ hosts/home-server/containers/trek.nix
@@ -8,7 +8,7 @@
 
   # Docker container
   virtualisation.oci-containers.containers.trek = {
-    image = "mauriceboe/trek:3.4.1";
+    image = "mauriceboe/trek:4.0.0-pre.1";
     ports = ["3001:3000"];
     volumes = [
       "/mnt/trek/data:/app/data"
