3ab58c558511249b6029226feb64fb46e4b7b30f
Salman Abuhaimed <salman.f.abuhaimed@gmail.com>
2026-08-13 23:18:39 +0300

    change ssh port

--- modules/ssh.nix
+++ modules/ssh.nix
@@ -1,7 +1,6 @@
 {lib, ...}: {
   services.openssh = {
     enable = true;
-    ports = [222];
     openFirewall = true;
     knownHosts = {
       alshaikh.publicKey = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIAAVPe6Tcsh7X89G6mjfurhez2Md9/VV6CxWIaDXdZfa";
